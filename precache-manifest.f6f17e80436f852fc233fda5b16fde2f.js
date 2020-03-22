93501b56cc64cfc3137ed2a73db9d842c206b2e0
Colin McCulloch <colinmucculloch@gmail.com>
2020-03-22 14:43:59 -0400

    Updates

---
+++ precache-manifest.f6f17e80436f852fc233fda5b16fde2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910f0ba8dc0e64bfabbed5fa11e1ec5e",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "f01abf4cfd0a8d141e07",
+    "url": "/to-do-list/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ab73ea7855a57d0412d8",
+    "url": "/to-do-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f01abf4cfd0a8d141e07",
+    "url": "/to-do-list/static/js/2.ded07362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.ded07362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab73ea7855a57d0412d8",
+    "url": "/to-do-list/static/js/main.bb5618b4.chunk.js"
+  },
+  {
+    "revision": "475cf87243a077512b27",
+    "url": "/to-do-list/static/js/runtime-main.88dcc749.js"
+  }
+]);
